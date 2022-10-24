8bf94dd7dd13d132babb49c231737cde94848833
OlyaKhromova <olya.hrmvlg@gmail.com>
2022-10-24 18:05:59 +0300

    Реализован таймер обратного отсчета. Если полььзователь успевает отгадать 10 ячеек за 60 секунд, то он победил. В противном случае - проиграл.

--- script/guessTheCell.js
+++ script/guessTheCell.js
@@ -6,7 +6,7 @@ function createTable() {
     /* создаем таблицу 10х10 */
     for (let i = 0; i < 10; i++) {
         var playgraundTableRows = document.createElement("TR");
-        playgraundTableRows.setAttribute("id", "myTr" + i);
+        playgraundTableRows.setAttribute("id","myTr" + i);
         document.getElementById("myTable").appendChild(playgraundTableRows);
 
         for (let i = 0; i < 10; i++) {
@@ -14,14 +14,20 @@ function createTable() {
             playgraundTableRows.appendChild(playgraundTableCols);
         }
     }
+    
+    /* задаем каждой ячейке индивидуальный Id и добавляем атрибуты функций*/
+    for (let i = 0; i < 100; i++) {
+            var cell = document.getElementsByTagName('TD')[i];
+            cell.setAttribute('id', i);
+        }
+}
 
-    /* задаем каждой ячейке индивидуальный Id */
+function setCellsAttribute() {
     for (let i = 0; i < 100; i++) {
         var cell = document.getElementsByTagName('TD')[i];
-        cell.setAttribute('id', i);
         cell.setAttribute('onclick', 'usersСellSelection(this.id)');
         //cell.setAttribute('onmouseover', 'usersСellSelection(this.id)');
-    }
+    }    
 }
 
 var start = 99;
@@ -29,11 +35,9 @@ var randomNumbersArray = [];
 while (start >= 0) {
     randomNumbersArray.push(start--);
 }
-
 function shuffle(randomNumbersArray) {
     let currentIndex = randomNumbersArray.length,
         randomIndex;
-
     while (currentIndex != 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
@@ -45,7 +49,6 @@ function shuffle(randomNumbersArray) {
             randomNumbersArray[currentIndex]
         ];
     }
-
     return randomNumbersArray;
 }
 
@@ -53,10 +56,10 @@ shuffle(randomNumbersArray);
 randomNumbersArray = randomNumbersArray.slice(0, 10);
 console.log(randomNumbersArray);
 
+/* перекрашиваем ячейки таблицы при нажатии */
 function usersСellSelection(clicked_id) {
     var cellId = parseInt(clicked_id);
     var cellToHighlight = document.getElementById(cellId);
-
     for (var i = 0; i < randomNumbersArray.length; i++) {
         if (cellToHighlight.classList.contains('green')) break;
         else if (randomNumbersArray[i] === cellId) {
@@ -69,10 +72,37 @@ function usersСellSelection(clicked_id) {
 }
 
 var counter = 0;
+let win = false;
 function Win() {
-    console.log(counter);
     counter++;
     if (counter == 10) {
-        console.log("Вы нашли все ячейки!");
+        win = true;
     }
+}
+
+let startBtn = document.querySelector('#start');
+let displayTimer = document.querySelector('.displayTimer');
+startBtn.addEventListener('click', function() {
+    let i = 60;
+    startBtn.disabled = true;
+    let timerId = setInterval(function () {
+        displayTimer.textContent = --i;
+        setCellsAttribute();
+        if(win == true){
+            alert("Победа! Вы успели найти все ячейки!");
+            whenTimeerStops(timerId);
+        }
+        if (i == 0 && win == false) {
+            alert('Вы проиграли! :(');
+            whenTimeerStops(timerId);
+        }
+    }, 1000);
+});
+
+function whenTimeerStops(timerId) {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    displayTimer.textContent = '';
+    counter = 0;    
+    location.reload();
 }
